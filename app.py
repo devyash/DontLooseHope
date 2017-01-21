b21376d940aab57406e83800e82b445692d02c3d
Devyash <devyashsanghai@gmail.com>
2017-01-21 04:09:51 -0500

    Basic working website server

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ CLARIFAI_APP_ID ="ptutSXCUdWgBfInt4WzBbVa2RZYUTkeZ2jli4jdE"
 
 
 #CLARIFAI_APP_ID=""
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 from clarifai.rest import ClarifaiApp
@@ -12,10 +12,9 @@ from clarifai.rest import ClarifaiApp
 app2 = ClarifaiApp()
 app2.tag_urls(['https://samples.clarifai.com/metro-north.jpg'])
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
 
 @app.route("/genderpage")
 def selectgender():
