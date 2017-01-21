abb488d43316a507e18d0d3baf020cc425e6ca4f
Devyash <devyashsanghai@gmail.com>
2017-01-21 02:54:03 -0500

    Initial structure of the app

---
+++ app.py
@@ -0,0 +1,26 @@
+
+#7Hhgsq7dGfibhHLC2YNQjEK_pMK9jB5N_HDqzLCM
+CLARIFAI_APP_ID ="ptutSXCUdWgBfInt4WzBbVa2RZYUTkeZ2jli4jdE"
+
+
+#CLARIFAI_APP_ID=""
+from flask import Flask
+app = Flask(__name__)
+
+from clarifai.rest import ClarifaiApp
+
+app2 = ClarifaiApp()
+app2.tag_urls(['https://samples.clarifai.com/metro-north.jpg'])
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/genderpage")
+def selectgender():
+	return "This will be the he and she page"
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ venv/lib/python2.7/site-packages/clarifai/rest/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from .client import ApiClient, ApiError, UserError, TokenError
+from .client import ClarifaiApp
+from .client import Model, Image, Concept
+from .client import InputSearchTerm, OutputSearchTerm, SearchQueryBuilder
+from .client import Geo, GeoPoint, GeoBox, GeoLimit
