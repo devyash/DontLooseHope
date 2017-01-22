1759a53efc92b9c2cc14c7b585d17b2e30b585a2
Devyash <devyashsanghai@gmail.com>
2017-01-21 22:05:01 -0500

    working

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from sqlalchemy.orm import sessionmaker
 from dB_setup import Base, MissingPeople,FoundPeople
 from clarifai.rest import Image as ClImage
 import json
+import time
+
 
 # Connect to Database and create database session
 engine = create_engine('sqlite:///DLH.db')
@@ -29,12 +31,23 @@ def findmissingperson():
 	if request.method == 'POST':
 
 		updatedb(request.form['name'],request.form['email'],request.form['phonenumber'])
-		trainclari(request.form['imageurl'],request.form['name'])
-		(name,error) = check(request.form['imageurl'])
-		#return render_template('displayresult.html', error=error, name=name)
-		return "Hello World"
+		(searchname,error) = check(request.form['imageurl'])
+
+		if(error!=True):
+			(name,address,phonenumber,email)=querydb(searchname)
+			trainclari(request.form['imageurl'],request.form['name'])
+	
+
+			return render_template('displaytrueresult.html', searchname=searchname, name= name,address=address,phonenumber=phonenumber,email=email)
+		else:
+			trainclari(request.form['imageurl'],request.form['name'])
+
+			return render_template('displayfalseresult.html', searchname=searchname)
+		# return "Hello World!"
 	else:
 		return render_template('findmissingperson.html')
+
+
 
 
 # @app.route("/reportmissingperson")
@@ -65,11 +78,10 @@ def check(url):
 #this function would call predict on the submited image and 
 # if the value is greater than 0.75
 # it would return error as false
-	image = ClImage(url)
-	parsed_input = model.predict([image])
-	#print(type(response))
-	#parsed_input = json.loads(response)
-	#print(parsed_input)
+	# image = ClImage(url=url)
+	print (url)
+	parsed_input = model.predict_by_url(url)
+	print(parsed_input)
 	if ((parsed_input['status'])['code'] != 10000):
 		print('Status:', response.status_code, 'Problem with the request. Exiting.')
 		return (0,0)
@@ -89,9 +101,15 @@ def check(url):
 
 def updatedb(name,email,phonenumber):
 	#this function is sqlalchemy commands to update the database
-	newMissingPeople=MissingPeople(email=email,name=email,phonenumber=phonenumber)
+	newMissingPeople=MissingPeople(email=email,phonenumber=phonenumber,name=name)
 	session.add(newMissingPeople)
 	session.commit()
 	return 0
+
+def querydb(searchname):
+	#this function queries the Found People Db and return the above 
+	#details of the person if the person is found 
+	foundperson = session.query(FoundPeople).filter_by(name = searchname).one()
+	return (foundperson.email,foundperson.phonenumber,foundperson.address,foundperson.name)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8001)
